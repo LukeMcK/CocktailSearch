1fb24773d8a991d9fd8c8dc174e494c9384ec45b
LukeMcK <lukemck110@gmail.com>
2023-05-11 19:13:25 -0400

    changed fetch from 'http' to 'https'

--- src/App.js
+++ src/App.js
@@ -1,65 +1,62 @@
-import {useState, useEffect} from 'react'
-import './App.css';
-import SearchIcon from './search.svg'
-import DrinkCard from './DrinkCard'
+import { useState, useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import DrinkCard from "./DrinkCard";
 
-const API_URL = "http://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 const App = () => {
-    const [drinks, setDrinks] = useState([])
-    const [searchTerm, setSearchTerm] = useState("");
+  const [drinks, setDrinks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleKeyPress = (event,title) => {
-        if(event.key === "Enter"){
-            searchDrinks(title);
-        }
+  const handleKeyPress = (event, title) => {
+    if (event.key === "Enter") {
+      searchDrinks(title);
     }
+  };
 
-    const searchDrinks = async (title) => {
-        const response = await fetch(`${API_URL}${title}`)
-        const data = await response.json();
+  const searchDrinks = async (title) => {
+    const response = await fetch(`${API_URL}${title}`);
+    const data = await response.json();
 
-        setDrinks(data.drinks)
-    }
+    setDrinks(data.drinks);
+  };
 
-    useEffect(()=>{
-        searchDrinks('');
-    },[])
+  useEffect(() => {
+    searchDrinks("");
+  }, []);
 
-    return (
-        <div className ="app">
-            <h1>Powered By TheCocktailDB</h1>
+  return (
+    <div className="app">
+      <h1>Powered By TheCocktailDB</h1>
 
-            <div className="search">
-                <input 
-                placeholder = "What Will You Be Having?"
-                value={searchTerm}
-                onChange={(e)=> setSearchTerm(e.target.value)}
-                onKeyDown = {(e)=>handleKeyPress(e,searchTerm)}
-                />
-                <img 
-                src ={SearchIcon} 
-                alt ="search"
-                onClick={()=>searchDrinks(searchTerm)}>
-                </img>
-            </div>
+      <div className="search">
+        <input
+          placeholder="What Will You Be Having?"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => handleKeyPress(e, searchTerm)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchDrinks(searchTerm)}
+        ></img>
+      </div>
 
-            {drinks?.length > 0 ? (
-                <div className="container">
-                    {drinks.map((drink) => (
-                        <DrinkCard key={drink.idDrink} drink={drink} />
-                    ))}
-                </div>
-            ) : (
-                <div className="empty">
-                    <h2>No drinks found</h2>
-                </div>
-            )}
-
-
-            
+      {drinks?.length > 0 ? (
+        <div className="container">
+          {drinks.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))}
         </div>
-    );
-}
+      ) : (
+        <div className="empty">
+          <h2>No drinks found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
